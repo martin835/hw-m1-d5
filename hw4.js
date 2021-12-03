953bd68ac852b816d1defd7c9bbe2eec21ee6ad3
martin835 <64438132+martin835@users.noreply.github.com>
2021-12-03 16:56:41 +0100

    excercise 6 improved

--- hw4.js
+++ hw4.js
@@ -139,7 +139,10 @@ console.log(9%7) */
     console.log("You must provide a positive number")
 
 
-  } else if (n%3 === 0 ) {
+  } else if (n%3 === 0 && n%7 === 0 ) {
+    console.log(`${n} is a multiple of both 3 and 7`) 
+  }
+    else if (n%3 === 0 ) {
     console.log(`${n} is a multiple of 3`) 
   } else if (n%7 === 0) {
     console.log(`${n} is a multiple of 7`)  
